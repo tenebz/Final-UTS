49abf5ca75ff4d2388887ac1407657c40c9bcf65
Ratri Ika Sulfiani <rratriika@gmail.com>
2025-06-15 21:24:49 +0700

    Update chatbot.php

--- html/chatbot.php
+++ html/chatbot.php
@@ -1,7 +1,8 @@
 <div class="row">
     <div class="col-12">
         <div class="chat-container">
-            <div class="chat-messages" id="chat-box"></div>
+            <div class="chat-header">Chatbot Transportasi Jakarta</div>
+            <div id="chat-box" class="chat-messages"></div>
             <div class="chat-input">
                 <input type="text" id="user-input" placeholder="Tulis pesan..." />
                 <button onclick="sendMessage()">Kirim</button>
